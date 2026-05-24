93e90815128b629e25e047959a28f1a68e549690
wanan-dev <zahartuksinkin@gmail.com>
2026-05-24 20:21:25 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -11,13 +11,11 @@ car1.driver = {
     "personal limitations": "No driving at night"
 };
 
-// 1.2.5 метод drive
 car1.drive = function () {
     console.log("I am not driving at night");
 };
 
 
-// 1.2.4 car2 через літерал
 let car2 = {
     color: "red",
     maxSpeed: 200,
@@ -31,13 +29,11 @@ let car2 = {
     }
 };
 
-// 1.2.6 метод drive
 car2.drive = function () {
     console.log("I can drive anytime");
 };
 
 
-// перевірка
 console.log("car1:");
 console.log(car1);
 car1.drive();
@@ -45,3 +41,45 @@ car1.drive();
 console.log("car2:");
 console.log(car2);
 car2.drive();
+
+function Truck(color, weight, avgSpeed, brand, model) {
+    this.color = color;
+    this.weight = weight;
+    this.avgSpeed = avgSpeed;
+    this.brand = brand;
+    this.model = model;
+}
+
+Truck.prototype.AssignDriver = function (name, nightDriving, experience) {
+    this.driver = {
+        name: name,
+        nightDriving: nightDriving,
+        experience: experience
+    };
+};
+
+Truck.prototype.trip = function () {
+    if (!this.driver) {
+        console.log("No driver assigned");
+    } else {
+        let message = "Driver " + this.driver.name;
+
+        if (!this.driver.nightDriving) {
+            message += " does not drive at night";
+        } else {
+            message += " drives at night";
+        }
+
+        message += " and has " + this.driver.experience + " years of experience";
+
+        console.log(message);
+    }
+};
+
+let truck1 = new Truck("white", 5000, 90, "Volvo", "FH16");
+
+truck1.trip();
+
+truck1.AssignDriver("Олексій", false, 5);
+
+truck1.trip();
