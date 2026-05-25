837a33926df58afa7214e4cde0b50bcaa53c0086
wanan-dev <zahartuksinkin@gmail.com>
2026-05-25 18:34:38 +0200

    Update script.js

--- lab8/script.js
+++ lab8/script.js
@@ -1,1 +1,6 @@
+let burgerBtn = document.getElementById("burgerBtn");
+let navMenu = document.getElementById("navMenu");
 
+burgerBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("open");
+});
