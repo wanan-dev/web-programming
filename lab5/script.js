4c6ed636dc3efe0714f10f6697dede23ffec4573
wanan-dev <zahartuksinkin@gmail.com>
2026-05-25 14:09:29 +0200

    Update script.js

--- lab5/script.js
+++ lab5/script.js
@@ -1,1 +1,11 @@
+function startGame() {
+    let difficulty = document.getElementById("difficulty").value;
+    let color = document.getElementById("color").value;
 
+    if (difficulty === "" || color === "") {
+        alert("Please choose difficulty and color!");
+        return;
+    }
+
+    alert("Game started!");
+}
