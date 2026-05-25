b7265a341c7eba485fc50722ae84ab1f9bb84c64
wanan-dev <zahartuksinkin@gmail.com>
2026-05-25 13:12:57 +0200

    Update sortLib.js

--- lab4/sortLib.js
+++ lab4/sortLib.js
@@ -33,3 +33,43 @@ SortLib.bubbleSort = function(arr, ascending = true) {
 
     return a;
 };
+
+SortLib.selectionSort = function(arr, ascending = true) {
+    let a = [...arr];
+    let comparisons = 0;
+    let swaps = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        let index = i;
+
+        for (let j = i + 1; j < a.length; j++) {
+
+            if (a[j] === undefined || a[index] === undefined) {
+                console.log("Found undefined element");
+                continue;
+            }
+
+            comparisons++;
+
+            let condition = ascending
+                ? a[j] < a[index]
+                : a[j] > a[index];
+
+            if (condition) {
+                index = j;
+            }
+        }
+
+        if (index !== i) {
+            [a[i], a[index]] = [a[index], a[i]];
+            swaps++;
+        }
+    }
+
+    console.log("Selection Sort:");
+    console.log("Result:", a);
+    console.log("Comparisons:", comparisons);
+    console.log("Swaps:", swaps);
+
+    return a;
+};
