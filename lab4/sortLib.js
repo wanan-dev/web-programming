48825188f319653e7e258dfac6bd3212aa64a626
wanan-dev <zahartuksinkin@gmail.com>
2026-05-25 13:16:25 +0200

    Update sortLib.js

--- lab4/sortLib.js
+++ lab4/sortLib.js
@@ -73,3 +73,45 @@ SortLib.selectionSort = function(arr, ascending = true) {
 
     return a;
 };
+
+SortLib.insertionSort = function(arr, ascending = true) {
+    let a = [...arr];
+    let comparisons = 0;
+    let swaps = 0;
+
+    for (let i = 1; i < a.length; i++) {
+        let key = a[i];
+        let j = i - 1;
+
+        while (j >= 0) {
+
+            if (a[j] === undefined || key === undefined) {
+                console.log("Found undefined element");
+                break;
+            }
+
+            comparisons++;
+
+            let condition = ascending
+                ? a[j] > key
+                : a[j] < key;
+
+            if (condition) {
+                a[j + 1] = a[j];
+                swaps++;
+                j--;
+            } else {
+                break;
+            }
+        }
+
+        a[j + 1] = key;
+    }
+
+    console.log("Insertion Sort:");
+    console.log("Result:", a);
+    console.log("Comparisons:", comparisons);
+    console.log("Swaps:", swaps);
+
+    return a;
+};
